d12f2c822e4e021826f350e9cad9d2d8d46f0f74
Sunny Islam <sunnyi@inventas.no>
2018-07-20 14:58:17 +0200

    Unity3D ROV underwater sim.
    Starting to edit translation controls

---
+++ Assets/Movement_translation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement_translation : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += 5.0f * transform.forward * Time.deltaTime;
+        transform.Rotate(0.0f, Input.GetAxis("Horizontal"), 0.0f);
+
+        Vector3 moveCamTo = transform.position - transform.forward * 10.0f + Vector3.up * 2.0f;
+        float bias = 0.5f;
+        Camera.main.transform.position = Camera.main.transform.position * bias + moveCamTo * (1 - bias);
+        Camera.main.transform.LookAt(transform.position + transform.forward * 30.0f);
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Eelume_demo
